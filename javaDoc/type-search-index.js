3c58fd90e40d0c1a40780158bc9513cfe5f56de0
9831121 <9831121@ceit.local>
2020-03-16 22:54:24 +0330

    javaDoc genarated

---
+++ javaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.company","l":"Book"},{"p":"com.company","l":"Borrow"},{"p":"com.company","l":"Library"},{"p":"com.company","l":"LibrarySystem"},{"p":"com.company","l":"TestLibrarySystem"},{"p":"com.company","l":"User"}]
